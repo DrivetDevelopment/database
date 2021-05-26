f8adacea19a8ebbf90bfc6432866d39e932e4a04
Slayer <51095261+GhostSlayer@users.noreply.github.com>
2021-05-26 15:17:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const config = require('../../config.json')
+const config = require('../config.json')
 const connection = mysql.createConnection({
   host     : config.mysql.host,
   user     : config.mysql.user,
