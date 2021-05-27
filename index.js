d7d00512846def40274b756b055cd2f655654712
Slayer <51095261+GhostSlayer@users.noreply.github.com>
2021-05-27 18:30:04 +0300

    rowQuery function

--- index.js
+++ index.js
@@ -20,6 +20,19 @@ const query = (sql, func) => {
   });
 }
 
+const rowQuery = (sql, func) => {
+  return new Promise((resolve, reject) => {
+
+    connection.query(sql, func, function(error, results, fields) {
+        if (error) {
+          console.error(error.sqlMessage);
+          return reject(new Error(error));
+        }
+        resolve(Object.values(JSON.parse(JSON.stringify(results))));
+    });
+  });
+}
+
 const showRows = (rows) => {
   if (rows) {
     return Object.values(JSON.parse(JSON.stringify(rows)))
@@ -32,4 +45,4 @@ const ping = async () => {
   return connection.state ? 'OK' : 'ONGELMIA'
 }
 
-module.exports = { query, showRows, ping };
+module.exports = { query, showRows, ping, rowQuery };
