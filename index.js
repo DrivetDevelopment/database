56ac4e63761c0214bed29d050b248e1081ed7318
Slayer <51095261+GhostSlayer@users.noreply.github.com>
2021-05-30 22:00:19 +0300

    Log on connect

--- index.js
+++ index.js
@@ -7,14 +7,22 @@ const connection = mysql.createConnection({
   database : config.databases.mysql.database,
 });
 
+connection.connect((err) => {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+});
+
 const query = (sql, func) => {
   return new Promise((resolve, reject) => {
-
     connection.query(sql, func, function(error, results, fields) {
         if (error) {
           console.error(error.sqlMessage);
           return reject(new Error(error));
         }
+
         resolve(results);
     });
   });
@@ -22,18 +30,20 @@ const query = (sql, func) => {
 
 const rowQuery = (sql, func) => {
   return new Promise((resolve, reject) => {
-
     connection.query(sql, func, function(error, results, fields) {
         if (error) {
           console.error(error.sqlMessage);
           return reject(new Error(error));
         }
+
         resolve(Object.values(JSON.parse(JSON.stringify(results))));
     });
   });
 }
 
 const showRows = (rows) => {
+  console.error('showRows: DEPRECATED: Use rowQuery instead')
+
   if (rows) {
     return Object.values(JSON.parse(JSON.stringify(rows)))
   } else {
@@ -42,7 +52,7 @@ const showRows = (rows) => {
 }
 
 const ping = async () => {
-  return connection.state ? 'OK' : 'ONGELMIA'
+  return connection.state ? true : false
 }
 
 module.exports = { query, showRows, ping, rowQuery };
